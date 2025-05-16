bae6b484cebb400b87ec2e1f637bbd68c71f4cb1
Tequali <dschuhknecht17@gmail.com>
2025-05-16 15:19:31 +0200

    added Django modules, files and minor changes to outline

--- nutrient_tracker/apps/user_info/enums.py
+++ nutrient_tracker/apps/user_info/enums.py
@@ -1,4 +1,22 @@
-GENDER: list = [["male", "MALE"], ["female", "FEMALE"]]
+class Gender:
+    """Enum class for user roles."""
+
+    MALE: str = "male"
+    FEMALE: str = "female"
+    OTHER: str = "other"
+
+    @classmethod
+    def get_gender(cls) -> list:
+        """Return a list of tuples containing the role and its name."""
+        return [
+            (cls.MALE, "MALE"),
+            (cls.FEMALE, "FEMALE"),
+            (cls.OTHER, "OTHER"),
+        ]
+
+    @classmethod
+    def get_default_gender(cls) -> str:
+        return cls.OTHER
 
 
 class Role:
@@ -16,3 +34,7 @@ class Role:
             (cls.USER, "USER"),
             (cls.GUEST, "GUEST"),
         ]
+
+    @classmethod
+    def get_default_role(cls) -> str:
+        return cls.GUEST
