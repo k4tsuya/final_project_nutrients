57d9a1e6959ea810a8abb8bda57ff37190990776
liang <liang.huang@dci-student.org>
2025-05-18 16:13:51 +0200

    Added users to admin panel and refractored code.

--- nutrient_tracker/apps/user_info/admin.py
+++ nutrient_tracker/apps/user_info/admin.py
@@ -1,3 +1,57 @@
+from typing import ClassVar
+
 from django.contrib import admin
 
+from .models import Favorite, History, User
+
 # Register your models here.
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display: ClassVar[list] = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "role",
+        "gender",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    ]
+
+    fieldsets = (
+        (
+            "Account info",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                    "email",
+                    "role",
+                ),
+            },
+        ),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "gender",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
