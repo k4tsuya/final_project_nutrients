74690cc76a11bf6f613ab5d5c35b48adcdcdbd64
Tequali <dschuhknecht17@gmail.com>
2025-05-20 10:28:15 +0200

    updated nutrients

--- docs/programming_style.md
+++ docs/programming_style.md
@@ -1,3 +1,14 @@
+
 # Commenting lines of code
 
 Longer comments go over the line it is talking about, unless its a minor reminder, then it can go behind the code.
+
+# Class structure
+
+## Django-related classes
+
+### Models
+
+* field names should be kept brief and on the point
+* boolean fields start with `is_`
+* Order of sections is: Table fields, Meta class info, dunder methods (e.g. : `__str__` )
--- nutrient_tracker/apps/nutrient_data/models.py
+++ nutrient_tracker/apps/nutrient_data/models.py
@@ -1,10 +1,40 @@
 """Django REST Framework models for nutrient data."""
 
-from rest_framework import serializers
+from django.db import models
+from apps.nutrient_data.enums import Category, Subcategory
 
 # Create your models here.
+
+from rest_framework import serializers
+
+
+# Create your models here.
+class Nutrient(models.Model):
+    name = models.CharField(max_length=100)
+    is_favorite = models.BooleanField(default=False)
+    # is_micronutrient determines if its macro or micro nutrients
+    category = models.CharField(  # macro, micro
+        max_length=100,
+        choices=Category.get_choices(),
+        default=Category.get_default_category(),
+    )
+    subcategory = models.CharField(  # vitamin, mineral, carbs, etc.
+        max_length=100,
+        choices=Subcategory.get_choices(),
+        default=Subcategory.get_default_subcategory(),
+    )
+    meassure_unit = models.CharField(max_length=2)  # g, mg, µg
+
+    class Meta:
+        verbose_name = "Nutrient"
+        verbose_name_plural = "Nutrients"
 
 
 # Serializers
 class NutrientSerializer(serializers.ModelSerializer):
     """Serializer for nutrient data."""
+
+
+# turn this into a rest API
+# add categories for the different forms of nutrients
+# add nutrient value from given databank
