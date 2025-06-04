9e53d64bf174ed993a187761c0c0269026f9be54
liang <liang.huang@dci-student.org>
2025-06-04 11:40:44 +0200

    added basic api throttling

--- nutrient_tracker/config/settings/base.py
+++ nutrient_tracker/config/settings/base.py
@@ -149,4 +149,12 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 2,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "3/min",
+        "user": "10/min",
+    },
 }
