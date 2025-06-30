fe3ab704ac1aca09da9844a675015d393ff4ef84
Tequali <dschuhknecht17@gmail.com>
2025-06-30 11:51:48 +0200

    updated the login page a tiny bit (wip still bugged)

--- nutrient_tracker/apps/user_info/views.py
+++ nutrient_tracker/apps/user_info/views.py
@@ -35,9 +35,9 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect("home")
     if request.method == "POST":
-        username = request.POST.get("username")
+        email = request.POST.get("email")
         password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return redirect("home")
